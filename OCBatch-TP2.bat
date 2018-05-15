881b5fd6f0a32326367f1a1d537bbb50a8fae006
wals38 <wals.38100@gmail.com>
2018-05-15 15:48:01 +0200

    20180515-1547-update

--- OCBatch-TP2.bat
+++ OCBatch-TP2.bat
@@ -1,4 +1,5 @@
 @echo off
+setlocal enableDelayedExpansion
 title OCBatch-TP2
 color 0a
 echo ^> OCBatch-TP2.bat
@@ -6,9 +7,6 @@ echo ^> OCBatch-TP2.bat
 :gameStart
 
 echo. & set /p niv=Choisissez un niveau de difficulté entre 1 et 10 : 
-echo. & echo L'ordinateur pense à un nombre... Trouvez le !
-
-set nbCoups=1
 
 if %niv% gtr 10 (
 	echo. & echo Il n'existe actuellement que 10 niveaux de difficulté ! Failed. & exit
@@ -66,18 +64,22 @@ set /a x=%RANDOM%* & echo %x%
 goto tryResult
 
 
+
 :tryResult
+echo. & echo L'ordinateur pense à un nombre... Trouvez le ! (%x%)
 set /p saisie=Saisissez un nombre : 
+set nbCoups=1
+
 :tryResult2
-set /a nbCoups = %nbCoups%+1
 if %saisie% equ %x% (
 	echo Bravo, vous avez trouvez en %nbCoups% coups !
 ) else (
+	set /a nbCoups = %nbCoups%+1
 	if %saisie% lss %x% (
-		set /p saisie=%nbCoups%. Le nombre est trop petit, recommencez : 
+		set /p saisie=!nbCoups!. Le nombre est trop petit, recommencez : 
 	) else (
-		set /p saisie=%nbCoups%. Le nombre est trop grand, recommencez : 
-	)
+		set /p saisie=!nbCoups!. Le nombre est trop grand, recommencez : 
+	) rem On utilise l'expansion retardée, avec les !, pour afficher le resultat de la variable après calcul par le préprocesseur
 	goto :tryResult2
 )
 
@@ -86,8 +88,8 @@ set score=\nVous avez trouvé le resultat (= %x%) en %nbCoups% coups.
 echo %score%>>result.txt
 
 
-echo. & set /p doItAg=Recommencer ? (O/N^) 
-if %doItAg% equ O (
+echo. & set /p doItAg=Recommencer ? (O/N^)
+if /i %doItAg% equ O (
 	goto gameStart
 ) else (
 	if doItAg equ Y goto gameStart
